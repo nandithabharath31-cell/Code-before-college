f11e0bdfb71024765b0d0ad6c522ff3603df7944
nandithabharath31 <nandithabharath31@gmail.com>
2026-08-09 00:27:31 +0530

    feat(c): implement account creation and account lookup

---
+++ C-Programming/Projects/mini-banking-system/README.md
@@ -0,0 +1,89 @@
+# 🏦 Mini Banking System
+
+A command-line banking application built using **C** to practice core programming concepts by developing a real-world application.
+
+## ✨ Features
+
+* Create and manage bank accounts
+* Secure login using account number and PIN
+* Check account balance
+* Deposit and withdraw money
+* Transfer money between accounts
+* View transaction history
+* Store account data using file handling
+* Input validation and error handling
+
+## 🧠 Concepts Used
+
+* Variables & Data Types
+* Conditions & Loops
+* Functions
+* Arrays & Strings
+* Structures
+* Pointers
+* Searching
+* File Handling
+* Input Validation
+
+## 📁 Project Structure
+
+```text
+mini-banking-system/
+│
+├── src/
+│   └── banking.c
+│
+├── data/
+├── README.md
+└── .gitignore
+```
+
+## 🧠 Program architecture
+                    BANKING SYSTEM
+                          │
+              ┌───────────┴───────────┐
+              ↓                       ↓
+        Load Accounts            Main Menu
+                                      │
+                    ┌─────────────────┼────────────────┐
+                    ↓                 ↓                ↓
+              Create Account        Login             Exit
+                                      │
+                                      ↓
+                              Verify Credentials
+                                      │
+                                      ↓
+                              Logged-in Menu
+                                      │
+            ┌─────────────┬───────────┼─────────────┐
+            ↓             ↓           ↓             ↓
+        Balance        Deposit     Withdraw      Transfer
+                                                    │
+                                                    ↓
+                                             Update accounts
+                                                    │
+                                                    ↓
+                                           Save to file
+## 🚧 Status
+
+**In Development**
+
+* [ ] Account creation
+* [ ] Login system
+* [ ] Balance management
+* [ ] Deposit & withdrawal
+* [ ] Money transfer
+* [ ] Transaction history
+* [ ] File storage
+* [ ] Input validation
+* [ ] Testing & improvements
+
+## 🎯 Goal
+
+To strengthen C programming and problem-solving skills by designing and building a complete application from scratch.
+
+## 👩‍💻 Author
+
+**Nanditha **
+
+Built as part of my C programming learning journey.
---
+++ C-Programming/Projects/mini-banking-system/src/banking.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+#define MAX_CUSTOMERS 100
+
+typedef struct customer_details
+{
+    int accountnumber;
+    char name[50];
+    int pin;
+    float balance;
+} user;
+
+int main()
+{
+    int totalcustomers = 0;
+    user accounts[MAX_CUSTOMERS];
+    //-------------------------MENU--------------------------------------
+    while (1)
+    {
+        printf("\n ================================ \n");
+        printf("         MINI BANKING SYSTEM \n");
+        printf(" ================================ \n");
+
+        printf("      1. Create Account \n");
+        printf("      2. Login \n");
+        printf("      3. Account Details \n");
+        printf("      4. Exit \n");
+
+        printf("Enter choice : ");
+        int ch;
+        scanf("%d", &ch);
+        if (ch != 1 && ch != 2 && ch != 3 && ch != 4)
+        {
+            printf("INVALID ENTRY , Try again ! ");
+        }
+
+        //--------------------CREATE ACCOUNT------------------------
+        if (ch == 1)
+        {
+            printf("\n-----------CREATE ACCOUNT-------------\n");
+            while (1)
+            {
+
+                printf("Enter account number : ");
+                scanf("%d", &accounts[totalcustomers].accountnumber);
+
+                //--------------checking for duplication---------------
+                int found = 0;
+                for (int i = 0; i < totalcustomers; i++)
+                {
+                    if (accounts[totalcustomers].accountnumber == accounts[i].accountnumber)
+                    {
+                        printf("!! ACCOUNT NUMBER ALREADY EXISTS !! \n");
+                        found = 1;
+                        break;
+                    }
+                }
+                if (found == 0)
+                {
+                    break;
+                }
+            }
+
+            printf("Enter user Name : ");
+            getchar(); // take \n character
+            fgets(accounts[totalcustomers].name, 50, stdin);
+            printf("Create PIN : ");
+            scanf("%d", &accounts[totalcustomers].pin);
+            printf("Enter initial balance : ");
+            scanf("%f", &accounts[totalcustomers].balance);
+
+            printf("\n~~~~~~~~~~~~~~~~~~~~~~~\n");
+            printf("    ACCOUNT CREATED \n");
+            printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+            printf(" Account Number : %d \n", accounts[totalcustomers].accountnumber);
+            printf(" Customer Name : %s ", accounts[totalcustomers].name);
+            printf("Balance : %.2f \n", accounts[totalcustomers].balance);
+
+            totalcustomers++;
+        }
+        //-------------------------ACCOUNT LOOKUP--------------------------------
+        else if (ch == 3)
+        {
+            printf("\n ---------------ACCOUNT DETAILS---------------------- \n");
+            int accnum;
+            int found = 0;
+            printf("Enter your account number : ");
+            scanf("%d", &accnum);
+            for (int i = 0; i < totalcustomers; i++)
+            {
+                if (accnum == accounts[i].accountnumber)
+                {
+                    printf(" \n Account Number : %d \n", accounts[i].accountnumber);
+                    printf(" Customer Name : %s", accounts[i].name);
+                    printf(" Balance : %.2f \n", accounts[i].balance);
+                    found = 1;
+                    break;
+                }
+            }
+            if (found == 0)
+            {
+                printf("ACCOUNT NOT FOUND");
+            }
+        }
+        //----------------------EXIT-----------------------------------
+        else if (ch == 4)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
